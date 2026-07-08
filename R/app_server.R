bccf80ac23f0efb8d03eec04fe773b677714b1de
Manuel Spínola <mspinola10@gmail.com>
2026-07-08 07:07:25 -0600

    Corrige permisos de ejecucion del binario de INLA

--- R/app_server.R
+++ R/app_server.R
@@ -5,5 +5,11 @@
 #' @import shiny
 #' @noRd
 app_server <- function(input, output, session) {
+  # Asegura permisos de ejecucion para el binario de INLA en este entorno
+  inla_bin_dir <- system.file("bin", "linux", package = "INLA")
+  if (dir.exists(inla_bin_dir)) {
+    Sys.chmod(list.files(inla_bin_dir, recursive = TRUE, full.names = TRUE), mode = "0755")
+  }
+
   mod_test_inla_server("test_inla_1")
 }
